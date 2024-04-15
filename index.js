f27d642dfbea300be1ab56393f6b4a34ff5376f7
mamtadevi19 <166403252+mamtadevi19@users.noreply.github.com>
2024-04-15 19:12:21 +0530

    variable updated

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log(" hello world");
+console.log("hello world 2");
+//console.log(2-2);
+console.log(4+3);
+console.log(3 * 7);
+console.log(12 / 2);
+name= "muskan";
+number = 3;
+console.log(name);
+console.log(number);
+sum = name + number;
+console.log(name);
+console.log(number);
+console.log(sum);
+//var keyword
+var name = "muskan";
+var age = 10;
+var sum = age + name;
+console.log(name);
+console.log(age);
+console.log(sum);
+{  
+     var name2 = "bharti";
+     let name = "muskan";
+     console.log(name2);
+     console.log(name);
+}
+name2 = "abc";
+name = "adcd";
+console.log(name2);
+console.log(name);
